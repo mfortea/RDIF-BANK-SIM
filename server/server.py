178763deffa3d2a023d27353bf4cddaaa2f2749d
Mateo Fortea Dugo <mfortea@MacBook-Air-de-Mateo.local>
2023-11-29 19:01:45 +0200

    Errors solved

--- server/server.py
+++ server/server.py
@@ -15,7 +15,7 @@ async def verify_user_card(user_card):
                 if user_card == user["username"]:  # Comparación en Base64
                     if not user["enabled"]:
                         return False, "User disabled", None
-                    return True, "", base64.b64decode(user_card).decode()  # Retorna nombre de usuario decodificado para uso posterior
+                return True, "", base64.b64decode(user_card).decode()  # Retorna nombre de usuario decodificado para uso posterior
             return False, "User not authorized", None
     except Exception as e:
         print(f"Error during user card verification: {e}")
