37b3816d2a3c68e2c2472b3724e8658e4dbd55e0
Mateo Fortea Dugo <mfortea@MacBook-Air-de-Mateo.local>
2023-12-04 11:15:23 +0200

    Fixed conection errors

--- server/server.py
+++ server/server.py
@@ -2,10 +2,17 @@ import asyncio
 import websockets
 import json
 import os
+import subprocess
 import ssl
 import signal
 from dotenv import load_dotenv
 import base64
+
+def clear_terminal():
+    if os.name == 'nt':
+        subprocess.run('cls', shell=True)
+    else:
+        subprocess.run('clear', shell=True)
 
 def load_users():
     with open("users.json", "r") as file:
@@ -15,58 +22,62 @@ def save_users(users):
     with open("users.json", "w") as file:
         json.dump(users, file, indent=4)
 
+users_file_lock = asyncio.Lock()
+
 async def manage_users(websocket, is_boss):
     if not is_boss:
         return
 
     while True:
         command = await websocket.recv()
-        users = load_users()
+        async with users_file_lock:
+            users = load_users()
+            if command == "list_users":
+                user_list = []
+                for user in users:
+                    decoded_username = base64.b64decode(user['username']).decode()
+                    user_info = f"Username: {decoded_username} - {'Is Boss' if user['boss'] else 'Not Boss'} - {'User enabled' if user['enabled'] else 'User disabled'}"
+                    user_list.append(user_info)
+                await websocket.send(json.dumps(user_list))
 
-        if command == "list_users":
-            user_list = []
-            for user in users:
-                # Decodificando el nombre de usuario de Base64 para mostrarlo
-                decoded_username = base64.b64decode(user['username']).decode()
-                user_info = f"Username: {decoded_username} - {'Is Boss' if user['boss'] else 'Not Boss'} - {'User enabled' if user['enabled'] else 'User disabled'}"
-                user_list.append(user_info)
-            await websocket.send(json.dumps(user_list))
-        elif command.startswith("modify_user"):
-            _, username, status = command.split()
-            # Codificando el nombre de usuario ingresado para la comparación
-            encoded_username = base64.b64encode(username.encode()).decode()
-            for user in users:
-                if user['username'] == encoded_username:
-                    user['enabled'] = status == 'enable'
+            elif command.startswith("modify_user"):
+                _, username, status = command.split()
+                encoded_username = base64.b64encode(username.encode()).decode()
+                decoded_username = base64.b64decode(encoded_username).decode()
+                for user in users:
+                    if user['username'] == encoded_username:
+                        user['enabled'] = status == 'enable'
+                        save_users(users)
+                        await websocket.send(f"User {decoded_username} updated.")
+                        break
+                else:
+                    await websocket.send(f"User {username} not found.")
+
+            elif command.startswith("delete_user"):
+                _, username = command.split()
+                encoded_username = base64.b64encode(username.encode()).decode()
+                original_count = len(users)
+                users = [user for user in users if user['username'] != encoded_username]
+                if len(users) < original_count:
                     save_users(users)
-                    await websocket.send(f"User {decoded_username} updated.")
-                    break
-            else:
-                await websocket.send(f"User {username} not found.")
-        elif command.startswith("delete_user"):
-            _, username = command.split()
-            encoded_username = base64.b64encode(username.encode()).decode()
-            original_count = len(users)
-            users = [user for user in users if user['username'] != encoded_username]
-            if len(users) < original_count:
-                save_users(users)
-                await websocket.send(f"User {username} deleted.")
-            else:
-                await websocket.send(f"User {username} not found.")
-        elif command.startswith("add_user"):
-            _, username, boss_str, enabled_str = command.split()
-            encoded_username = base64.b64encode(username.encode()).decode()
-            is_boss = boss_str == 'yes'
-            is_enabled = enabled_str == 'yes'
-            # Verifica si el usuario ya existe
-            if any(user["username"] == encoded_username for user in users):
-                await websocket.send("Error: The user already exists")
-            else:
-                users.append({"username": encoded_username, "enabled": is_enabled, "boss": is_boss})
-                save_users(users)
-                await websocket.send(f"User {username} added.")
-        elif command == "exit":
-            break
+                    await websocket.send(f"User {username} deleted.")
+                else:
+                    await websocket.send(f"User {username} not found.")
+
+            elif command.startswith("add_user"):
+                _, username, boss_str, enabled_str = command.split()
+                encoded_username = base64.b64encode(username.encode()).decode()
+                is_boss = boss_str == 'yes'
+                is_enabled = enabled_str == 'yes'
+                # Verifica si el usuario ya existe
+                if any(user["username"] == encoded_username for user in users):
+                    await websocket.send("Error: The user already exists")
+                else:
+                    users.append({"username": encoded_username, "enabled": is_enabled, "boss": is_boss})
+                    save_users(users)
+                    await websocket.send(f"User {username} added.")
+            elif command == "exit":
+                break
 
 async def verify_user_card(user_card):
     user_card = user_card.strip()  # Eliminar espacios en blanco
@@ -77,12 +88,13 @@ async def verify_user_card(user_card):
                 user_name = user["username"].strip()  # Eliminar espacios en blanco
                 if user_card == user_name:
                     if not user["enabled"]:
-                        return False, "User disabled", None
+                        return False, "User disabled", user_card, False
                     return True, "USER_OK", base64.b64decode(user_card).decode(), user["boss"]
-            return False, "User not authorized", None
+            return False, "User not authorized", user_card, False
     except Exception as e:
         print(f"Error during user card verification: {e}")
-        return False, "User card verification error", None
+        return False, "User card verification error", user_card, False
+
 
 auth_card_content_encrypted = base64.b64encode("SharedAuthData".encode()).decode()  # Reemplazar con tu valor real
 
@@ -106,51 +118,70 @@ async def authenticate_user(auth_card, auth_card_content):
 
 
 async def payment_processor(websocket, path):
-    user_card_data = await websocket.recv()
-    user_card = json.loads(user_card_data)["user_card"]
+    username = "UNKNOWN USER"
+    connection_active = True  # Flag para controlar la vida de la conexión
 
-    user_valid, message, username, is_boss = await verify_user_card(user_card)
-    if not user_valid:
-        await websocket.send(message)
-        return
-    else:
-        boss_status = 'boss' if is_boss else 'not_boss'
-        await websocket.send(f"USER_OK;{boss_status}")
-
-    auth_card_data = await websocket.recv()
-    auth_card = json.loads(auth_card_data)["auth_card"]
-
-    # Asegúrate de definir o tener accesible auth_card_content
-    auth_card_content = "SharedAuthData"  # Reemplaza esto con el valor real esperado
-
-    auth_valid, message = await authenticate_user(auth_card, auth_card_content)
-
-    if not auth_valid:
-        await websocket.send(message)
-        return
-    else:
-        await websocket.send("AUTH_OK")
-
-    if is_boss:
-        await manage_users(websocket, is_boss)
-
-    print(f"\n-> {username} CONNECTED")
-    connected_clients.add(websocket)
     try:
-        async for message in websocket:
-            # Handling user choices here (implementation pending)
-            pass
+        while connection_active:
+            try:
+                # Espera por mensajes del cliente pero con un timeout
+                task = asyncio.wait_for(websocket.recv(), timeout=1.0)
+                user_card_data = await task
+                user_card = json.loads(user_card_data)["user_card"]
+
+                user_valid, message, username, is_boss = await verify_user_card(user_card)
+                if not user_valid:
+                    await websocket.send(message)
+                    continue 
+
+                boss_status = 'boss' if is_boss else 'not_boss'
+                await websocket.send(f"USER_OK;{boss_status}")
+
+                auth_card_data = await websocket.recv()
+                auth_card = json.loads(auth_card_data)["auth_card"]
+
+                auth_valid, message = await authenticate_user(auth_card, "SharedAuthData")
+
+                if not auth_valid:
+                    await websocket.send(message)
+                    continue
+
+                await websocket.send("AUTH_OK")
+                print(f"\n-> USER {username} CONNECTED")
+
+                if is_boss:
+                    await manage_users(websocket, is_boss)
+
+            except asyncio.TimeoutError:
+                # Si no hay mensajes, simplemente continúa el bucle
+                continue
+            except websockets.exceptions.ConnectionClosed:
+                # Si la conexión se cierra, actualiza el flag y rompe el bucle
+                connection_active = False
+
+    except websockets.exceptions.ConnectionClosedOK:
+        print(f"-> USER {username} DISCONNECTED")
+    except websockets.exceptions.ConnectionClosedError as e:
+        print(f"-> USER {username} DISCONNECTED with error: {e}")
     finally:
-        connected_clients.remove(websocket)
-        print(f"-> {username} DISCONNECTED")
+        if websocket in connected_clients:
+            connected_clients.remove(websocket)
+        print(f"-> USER {username} DISCONNECTED")
 
 
 # Función para manejar el cierre del servidor
-async def shutdown(server, event):
+async def shutdown(server, loop):
     print("\n\nSERVER CLOSED")
     server.close()
     await server.wait_closed()
-    event.set()
+    tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
+    for task in tasks:
+        task.cancel()
+        try:
+            await task
+        except asyncio.CancelledError:
+            pass
+    loop.stop()
 
 if __name__ == "__main__":
     current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -171,26 +202,30 @@ if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
 
-    stop_event = asyncio.Event()
-
-    start_server = websockets.serve(
-        payment_processor, 
-        SERVER_MODE, 
-        PORT,
-        ssl=ssl_context,
-        ping_interval=300,
-        ping_timeout=300,
-        close_timeout=300
-    )
-    print("* SERVER RUNNING...")
-    server = loop.run_until_complete(start_server)
-
     def signal_handler():
-        asyncio.create_task(shutdown(server, stop_event))
+        asyncio.ensure_future(shutdown(server, loop))
 
     loop.add_signal_handler(signal.SIGINT, signal_handler)
 
     try:
-        loop.run_until_complete(stop_event.wait())
+        start_server = websockets.serve(
+            payment_processor, 
+            SERVER_MODE, 
+            PORT,
+            ssl=ssl_context,
+            ping_interval=300,
+            ping_timeout=300,
+            close_timeout=300
+        )
+        clear_terminal()
+        print("* SERVER RUNNING...")
+        server = loop.run_until_complete(start_server)
+
+        loop.run_forever()
+    except OSError as e:
+        if e.errno == 48:  # Número de error para 'address already in use'
+            print("PORT ALREADY IN USE")
+        else:
+            print(f"OSError: {e}")
     finally:
-        loop.close()
+        loop.close()    
