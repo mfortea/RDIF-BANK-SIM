b30bdb0dd2d3651db01f854b1bdf16410d70ecbf
Mateo Fortea Dugo <mfortea@MacBook-Air-de-Mateo.local>
2024-01-16 16:36:01 +0200

    Added client & server

--- security_changes/client.py
+++ security_changes/client.py
@@ -54,9 +54,9 @@ def send_card_data_to_server(ws, card_data, username):
     # Empaquetar los datos en un objeto JSON y enviarlos al servidor
         message = json.dumps({
         'username': username,
-        'aes_key': card_data[0],
-        'card_nonce': card_data[1],
-        'card_encrypted_password_hex': card_data[2]
+        'aes_key': card_data[0] + card_data[1],
+        'card_nonce': card_data[2],
+        'card_encrypted_password_hex': card_data[3]
         })
         ws.send(message)
 
